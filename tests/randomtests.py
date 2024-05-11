22be943963d08132f79aa88951ab0d9730e51c10
Maksim Kholod <125661351+xMaksIce@users.noreply.github.com>
2024-05-11 20:35:46 +0600

    Add MultipleTest

--- tests/randomtests.py
+++ tests/randomtests.py
@@ -1,4 +1,5 @@
 from ortools.algorithms.python import knapsack_solver
+from algorithm.genetic import Genetic
 from numpy.random import randint
 
 class Tests:
@@ -21,3 +22,23 @@ class Tests:
         lib_result = lib_value, packed_items
 
         return knapsack, capacity, genetic_result, lib_result
+    
+
+    def RandomMultipleAttempts(items: int = 3, attempts: int = 1) -> int:
+        values, weights = randint(low=1, high=100, size=(2, items))
+        capacity = randint(low=min(weights), high=sum(weights))
+        knapsack = list(zip(values, weights))
+        genetic_value = 0
+        lib_value = 0
+    
+        for k in range(attempts):
+            genetic = Genetic()
+            lib = knapsack_solver.KnapsackSolver(
+            knapsack_solver.SolverType.KNAPSACK_MULTIDIMENSION_BRANCH_AND_BOUND_SOLVER,
+            "KnapsackExample",
+            )
+            genetic_value += genetic.solve(knapsack, capacity)[0]
+            lib.init(values, [weights], [capacity])
+            lib_value += lib.solve()
+    
+        return knapsack, capacity, genetic_value, lib_value

472cf27cbe05aeee9041fde99fb4627802c1d762
Maksim Kholod <125661351+xMaksIce@users.noreply.github.com>
2024-05-19 18:58:37 +0600

    Update test.py

--- tests/test.py
+++ tests/test.py
@@ -7,3 +7,7 @@ print("Library Result:", lib_result)
 knapsack, capacity, genetic_value, lib_value = Tests.RandomMultipleAttempts(items=30, attempts=10)
 print("Genetic Value:", genetic_value)
 print("Library Value:", lib_value)
+
+genetic_results, lib_results = Tests.RandomMultipleCases(items=100, cases=10)
+print("Genetic Results:", genetic_results)
+print("Library Results:", lib_results)
